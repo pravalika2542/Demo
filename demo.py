405257fa318a1e50705de65811d110f06b58cbe7
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 11:52:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='food')        
 st.markdown("## Food")                   
 st.write("- Fried Rice")    
-st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image="https://yellowchilis.com/wp-content/uploads/2020/12/indian-vegetable-egg-masala-fried-rice-500x375.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.write("- Chiken")
 st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.image("./fried_rice.htm")
