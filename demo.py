2c8df377eb5ff95b71a8ae9b50767a0e60e3a2bc
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 11:40:39 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.set_page_config(page_title='cats')        
 st.markdown("## Food")                   
 st.write("Fried Rice")    
-st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image("fried_rice.htm", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.write("Chiken")
-st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image("chicken", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
