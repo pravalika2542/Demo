35850fc078462f73e61bd802fe912f1a91a67fb0
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 11:49:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.set_page_config(page_title='food')        
 st.markdown("## Food")                   
 st.write("- Fried Rice")    
-st.image("./fried_rice.htm")
+st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.write("- Chiken")
-st.image("./chicken.jpg")
+st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
