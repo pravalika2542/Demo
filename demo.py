129cc535c8be0540d19e5363110da44ddf28ecaa
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 12:08:54 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
 import streamlit as st
 st.set_page_config(page_title='food')        
-st.markdown("## Food")                   
-st.write("- Fried Rice")    
-st.image(image="https://yellowchilis.com/wp-content/uploads/2020/12/indian-vegetable-egg-masala-fried-rice-500x375.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-st.write("- Chiken")
-st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-st.image("./fried_rice.htm")
+st.header("My Favourite Food")     
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Fried Rice")  
+  st.image(image="https://yellowchilis.com/wp-content/uploads/2020/12/indian-vegetable-egg-masala-fried-rice-500x375.jpg", caption="Fried Rice", width=300, use_column_width=True, )
+with col2:
+  st.subheader("Chicken")
+  st.image(image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg/1200px-Tandoori_chicken_laccha_piyaz1_%2836886283595%29.jpg", caption="Chicken", width=300, use_column_width=True, )
+
