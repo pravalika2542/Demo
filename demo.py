6a915cd8d5ec44427e1afa9200997fc04f97ba66
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 11:32:01 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title='cats')        
+st.markdown("## Types of cats")                   
+st.write("Persian Cat")                        
+st.write("White Cat")
+st.iamge("./AI_part_1.jpg")
