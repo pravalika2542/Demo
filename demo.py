d432e24865da353f1507bc24d9f49e3703a71e72
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2023-12-29 11:46:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='food')        
 st.markdown("## Food")                   
-st.write("Fried Rice")    
+st.write("- Fried Rice")    
 st.image("./fried_rice.htm")
-st.write("Chiken")
+st.write("- Chiken")
 st.image("./chicken.jpg")
