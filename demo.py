53c6f3a7a6ad5e2aa3e17d82774eb477182f599b
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-04 17:46:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title='bmi')        
-st.title("My Favourite Food")     
+st.title("Welcome to BMI Calculator")     
+
+weidht=st.number_input("Enter your weight(in kgs):")
