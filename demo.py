888b756d01352eed9880da9bd5d6ab5215c84f63
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-04 17:58:15 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -23,3 +23,19 @@ else:
     bmi = weight / (((height/3.28))**2)
   except:
     st.text("Enter some value of height")
+if(st.button('Calculate BMI')):
+ 
+    # print the BMI INDEX
+    st.text("Your BMI Index is {}.".format(bmi))
+ 
+    # give the interpretation of BMI index
+    if(bmi < 16):
+        st.error("You are Extremely Underweight")
+    elif(bmi >= 16 and bmi < 18.5):
+        st.warning("You are Underweight")
+    elif(bmi >= 18.5 and bmi < 25):
+        st.success("Healthy")
+    elif(bmi >= 25 and bmi < 30):
+        st.warning("Overweight")
+    elif(bmi >= 30):
+        st.error("Extremely Overweight")
