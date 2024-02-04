448dcb45fa683cbb57feb7ca6ac43483830583bd
pravalika2542 <124230743+pravalika2542@users.noreply.github.com>
2024-02-04 17:50:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,3 +5,9 @@ st.title("Welcome to BMI Calculator")
 weight=st.number_input("Enter your weight(in kgs):")
 
 height=st.radio("Select your height format:",('Cms','meters','feet'))
+if(height==Cms):
+  st.number_input("Centimeters:")
+elif(height==meters):
+  st.number_input("Meters:")
+else:
+  st.number_input("Feet:")
